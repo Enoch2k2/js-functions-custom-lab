8422c823d8abf6d68b7c7bca22067c9c79a69592
Enoch2k2 <enoch2k2@hotmail.com>
2022-05-26 15:56:30 -0500

    added solution

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 // define your functions here
 function doMath(num1, num2, equation) {
-
+  if(equation === "+") {
+    return num1 + num2;
+  }
+  else if(equation === "-") {
+    return num1 - num2;
+  }
+  else if(equation === "*") {
+    return num1 * num2;
+  }
+  else {
+    return num1 / num2;
+  }
 }
 
 function sayHelloToGrandma(string) {
-
+  if(string.toUpperCase() === string) {
+    return 'Not since 1939!'
+  }
+  else if(string.toLowerCase() === string) {
+    return 'Huh? Speak up!'
+  } else {
+    return 'I love you too!'
+  }
 }
 
 /** Don't delete this line **/
